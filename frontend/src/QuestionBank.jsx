db0a3450bf3bde03a42655bde207ba9f489fe1a0
mandykqh <>
2023-09-10 12:55:18 +0800

    Create question bank headings

---
+++ frontend/src/QuestionBank.jsx
@@ -0,0 +1,17 @@
+export default function QuestionBank() {
+    return (
+        <div>
+            <h1>Question Bank</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Category</th>
+                        <th>Complexity</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    )
+}
