b0a0e6114f273dfbdf96fe4adaa49d84a2e6e8d8
mandykqh <>
2023-09-12 10:38:03 +0800

    Style description popup window to overlay

--- frontend/src/DescriptionPopup.jsx
+++ frontend/src/DescriptionPopup.jsx
@@ -1,11 +1,14 @@
+import "./DescriptionPopup.css";
 export default function DescriptionPopup({ qn, idx, toClose }) {
     console.log("Received questionDescription:", qn);
 
     return (
-        <div>
-            <h3>ID{idx + 1} Question description</h3>
-            <p>{qn}</p>
-            <button onClick={toClose}>Close</button>
+        <div className="descriptionPopup">
+            <div className="content">
+                <h3>ID{idx + 1} Question description</h3>
+                <p>{qn}</p>
+                <button onClick={toClose}>Close</button>
+            </div>
         </div>
     )
 }
