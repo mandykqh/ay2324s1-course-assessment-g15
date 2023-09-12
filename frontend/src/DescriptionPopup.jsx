05bd8a12c6f49f4cdd817d2237afa17cef1a140b
mandykqh <>
2023-09-12 11:34:36 +0800

    Display question title and complexity in description popup

--- frontend/src/DescriptionPopup.jsx
+++ frontend/src/DescriptionPopup.jsx
@@ -1,14 +1,13 @@
 import "./DescriptionPopup.css";
 export default function DescriptionPopup({ qn, idx, toClose }) {
-    console.log("Received questionDescription:", qn);
-
     return (
         <div className="descriptionPopup">
             <div className="content">
-                <h3>ID{idx + 1} Question description</h3>
-                <p>{qn}</p>
-                <button onClick={toClose}>Close</button>
+                <h3 className="title">ID{idx + 1} {qn.title}</h3>
+                <h4 className="complexity">{qn.complexity}</h4>
+                <p>{qn.description}</p>
+                <button onClick={() => toClose(qn, idx)}>Close</button>
             </div>
-        </div>
+        </div >
     )
 }
