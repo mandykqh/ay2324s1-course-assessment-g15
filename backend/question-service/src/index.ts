d2a1086c8e8acfa75f4998203141c5e5341ff22d
Dylan Sng <dsja612@gmail.com>
2023-09-13 20:50:28 +0800

    feat: added skeletal backend server

---
+++ backend/question-service/src/index.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+// import router from './router'
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 8080, () => {
+    console.log('Server listening on port 8080');
+});
+
+const MONGOURL = process.env.MONGOURL;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGOURL);
+mongoose.connection.on('error', () => {
+    console.log('MongoDB connection error. Please make sure MongoDB is running.');
+    process.exit();
+});
+
+// app.use('/', router());
