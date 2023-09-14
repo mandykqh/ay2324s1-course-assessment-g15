5de06c52070f613c86a8eb03e69455e16cf85a4a
Dylan Sng <dsja612@gmail.com>
2023-09-14 19:42:41 +0800

    chore: removed unnecessary packages

--- backend/question-service/src/index.ts
+++ backend/question-service/src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
@@ -16,7 +15,6 @@ app.use(cors({
 }));
 
 app.use(compression());
-app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
