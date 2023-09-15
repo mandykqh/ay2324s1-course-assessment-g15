b072122a2df97e66e2a4636cf8624fdcf553fa42
Wei-Xinn <E0544325@u.nus.edu>
2023-09-15 17:51:25 +0800

    feat: populate DescriptionModal

--- frontend/src/pages/QuestionPage.tsx
+++ frontend/src/pages/QuestionPage.tsx
@@ -1,14 +1,49 @@
+import './QuestionPage.css'
 import { questionSet } from "../MockData";
 import QuestionTable from "../question/QuestionTable";
+import DescriptionModal from "../question/descriptionModal/DescriptionModal";
+import { useEffect, useState } from 'react';
+import { QuestionString } from '../models/Question';
+
+let qn = { title: '', category: '', complexity: '', description: '' };
 
 const QuestionPage = () => {
+
+  const [questions, setQuestions] = useState<QuestionString[]>([]);
+  const [descModalIsVisible, setDescModalIsVisible] = useState(false);
+  const [currentQuestionId, setCurrentQuestionId] = useState('0');
+
+  useEffect(() => { setQuestions(questionSet); }, []);
+
+
+  const selectedQuestion = questions.filter(i => i.id === currentQuestionId)[0];
+  if (selectedQuestion !== undefined) {
+    qn.title = (selectedQuestion.title);
+    qn.category = (selectedQuestion.categories);
+    qn.complexity = (selectedQuestion.complexity);
+    qn.description = (selectedQuestion.description);
+  }
+
   return (
-    <QuestionTable
-      data={questionSet}
-      isDeleting={false}
-      viewDescriptionHandler={(id: string) => { }}
-      deleteHandler={(id: string) => { }}
-    />
+    <div id='question-page-container'>
+      <DescriptionModal
+        isVisible={descModalIsVisible}
+        title={qn.title}
+        category={qn.category}
+        complexity={qn.complexity}
+        description={qn.description}
+        closeHandler={() => { setDescModalIsVisible(false) }}
+      />
+      <QuestionTable
+        data={questionSet}
+        isDeleting={false}
+        viewDescriptionHandler={(id: string) => {
+          setCurrentQuestionId(id);
+          setDescModalIsVisible(true)
+        }}
+        deleteHandler={(id: string) => { }}
+      />
+    </div>
   );
 }
 
--- frontend/src/question/descriptionModal/DescriptionModal.tsx
+++ frontend/src/question/descriptionModal/DescriptionModal.tsx
@@ -1,23 +1,60 @@
-import "./DescriptionPopup.css";
-import Question from "./assets/models/Question";
+import './DescriptionModal.css';
+import { Box, Input, Modal, Typography } from "@mui/material";
 
 interface Props {
-    qn: Question;
-    idx: number;
-    toClose: (qn: Question, idx: number) => {}
+  isVisible: boolean;
+  title: string;
+  category: string;
+  complexity: string;
+  description: string
+  closeHandler: () => void;
 }
 
-const DescriptionPopup: React.FC<Props> = ({ qn, idx, toClose }) => {
+const DescriptionModal: React.FC<Props> =
+  ({ isVisible, title, category, complexity, description, closeHandler }) => {
     return (
-        <div className="descriptionPopup">
-            <div className="content">
-                <h3 className="title">ID{idx + 1} {qn.title}</h3>
-                <h4 className="complexity">{qn.complexity}</h4>
-                <p>{qn.description}</p>
-                <button onClick={() => toClose(qn, idx)}>Close</button>
+      <Modal
+        open={isVisible}
+        onClose={() => { }}
+      >
+        <Box id='modal-box'>
+          <div id='inner-modal-container'>
+            <div onClick={closeHandler} style={{ width: '20px', height: '20px', alignSelf: 'end' }}>
+              {/* TODO change this button to image */}
+              <b>X</b>
             </div>
-        </div >
-    )
-}
+            <div id='top-row'>
+              <Typography id="modal-title" variant="h5" component="h2" color={"white"}>
+                {title}
+              </Typography>
+            </div>
+            <div id='middle-row'>
+              <div id='middle-left-container'>
+                <Typography id="category-label" component="h3" color={"white"} textAlign={'left'}>
+                  {`Category: ${category}`}
+                </Typography>
+              </div>
+              <div id='middle-right-container'>
+                <Typography id="complexity-label" component="h3" color={"white"}>
+                  {`Complexity: ${complexity}`}
+                </Typography>
+              </div>
+            </div>
+            <div id='bottom-row'>
+              <Input
+                id='description-input'
+                multiline
+                minRows={20}
+                maxRows={20}
+                fullWidth
+                disabled
+                value={description}
+              />
+            </div>
+          </div>
+        </Box>
+      </Modal>
+    );
+  }
 
-export default DescriptionPopup;
+export default DescriptionModal;
