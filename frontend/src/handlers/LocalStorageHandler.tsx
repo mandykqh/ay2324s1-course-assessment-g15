d8e931a28411eff9faa8a3ed96fc98aff217f889
Wei-Xinn <E0544325@u.nus.edu>
2023-09-15 18:08:03 +0800

    feat: add LocalStorageHandler

---
+++ frontend/src/handlers/LocalStorageHandler.tsx
@@ -0,0 +1,25 @@
+import { QuestionString } from "../models/Question";
+
+class LocalStorageHandler {
+
+  static saveQuestion(qnArr: QuestionString[]) {
+    window.localStorage.setItem("Questions", JSON.stringify(qnArr));
+  }
+
+  static loadQuestion() {
+    return JSON.parse(window.localStorage.getItem("Questions") || '{}');
+  }
+
+  //Temp method for getting QuestionID
+  static getNextQuestionId() {
+    if (localStorage.getItem("nextId") === null) {
+      localStorage.setItem("nextId", '100')
+      return '100';
+    }
+    const nextId = (localStorage.getItem("nextId")!); //USING NON NULL OPERATOR
+    localStorage.setItem("nextId", (parseInt(nextId) + 1).toString());
+    return nextId;
+  }
+}
+
+export default LocalStorageHandler;
