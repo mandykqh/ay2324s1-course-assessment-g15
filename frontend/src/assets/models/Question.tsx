aac4adc7e25591900ed27c4ed0a1910d0ec87309
Wei-Xinn <E0544325@u.nus.edu>
2023-09-15 17:14:05 +0800

    chore: refactor Question model

--- frontend/src/assets/models/Question.tsx
+++ frontend/src/assets/models/Question.tsx
@@ -1,15 +1,59 @@
-export default class Question {
-    id: string;
-    title: string;
-    description: string;
-    category: string;
-    complexity: string;
+enum Category {
+    "Strings",
+    "Data Structures",
+    "Algorithms"
+}
 
-    constructor(id: string, title: string, description: string, category: string, complexity: string) {
+enum Complexity {
+    "Easy",
+    "Medium",
+    "Hard"
+}
+
+interface QuestionString {
+    id: string,
+    title: string,
+    complexity: string,
+    categories: string,
+    description: string,
+    link: string
+}
+
+function parseCategories(inputString: string) {
+    return inputString.split(',')
+        .map((c) => Category[c.trim() as keyof typeof Category]);
+}
+
+function parseComplexity(inputString: string) {
+    return Complexity[inputString as keyof typeof Complexity];
+}
+
+class Question {
+    id: Number;
+    title: string;
+    categories: Category[];
+    complexity: Complexity;
+    link: string;
+    description: string;
+
+    constructor(id: Number, title: string, categories: string, complexity: string, link: string, description: string) {
         this.id = id;
         this.title = title;
+        this.categories = parseCategories(categories);
+        this.complexity = parseComplexity(complexity);
+        this.link = link;
         this.description = description;
-        this.category = category;
-        this.complexity = complexity;
     }
+
+    public getCategoriesString() {
+        return this.categories.map((c) => Category[c]).join(', ');
+    }
+
+    public getComplexityString() {
+        return Complexity[this.complexity];
+    }
+
 }
+
+export { Question, Category, Complexity };
+export type { QuestionString };
