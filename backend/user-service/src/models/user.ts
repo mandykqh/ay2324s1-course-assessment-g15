5aa5c8fba40eb042dcfe73c343682ab7c5b5d7bf
Dylan Sng <dsja612@gmail.com>
2023-09-21 20:42:14 +0800

    fix: added more logical constraints to user database model

--- backend/user-service/src/models/user.ts
+++ backend/user-service/src/models/user.ts
@@ -15,20 +15,25 @@ User.init(
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
+      allowNull: false 
     },
     username: {
       type: DataTypes.STRING,
       unique: true,
+      allowNull: false,
     },
     email: {
       type: DataTypes.STRING,
       unique: true,
+      allowNull: false,
     },
     password: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     role: {
       type: DataTypes.ENUM('ADMIN', 'USER'),
+      allowNull: false,
     },
   },
   {
