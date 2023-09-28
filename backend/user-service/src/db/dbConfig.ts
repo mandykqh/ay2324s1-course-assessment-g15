f44e691272e41f5a2feac15c180c114907154237
adriel kee <adrielkee@gmail.com>
2023-09-28 18:03:10 +0800

    feat: Update dbconfig file for local db

--- backend/user-service/src/db/dbConfig.ts
+++ backend/user-service/src/db/dbConfig.ts
@@ -1,6 +1,17 @@
 import { Sequelize } from 'sequelize-typescript';
 
+/*
+#For connecting to elephant sql cloud db
 const sequelize = new Sequelize(process.env.POSTGRESURL, {
+  dialect: 'postgres',
+});
+*/
+
+const sequelize = new Sequelize({
+  database: process.env.PG_DB,
+  username: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST, // This should match the service name in your Docker Compose file
   dialect: 'postgres',
 });
 
