c105ca2788063d84dfb15a287de43aedb75b748e
adriel kee <adrielkee@gmail.com>
2023-10-04 16:00:40 +0800

    Chore: Edit Readme and remove cloud user-related configs

--- backend/user-service/src/db/dbConfig.ts
+++ backend/user-service/src/db/dbConfig.ts
@@ -1,17 +1,10 @@
 import { Sequelize } from 'sequelize-typescript';
 
-/*
-#For connecting to elephant sql cloud db
-const sequelize = new Sequelize(process.env.POSTGRESURL, {
-  dialect: 'postgres',
-});
-*/
-
 const sequelize = new Sequelize({
   database: process.env.PG_DB,
   username: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST, // This should match the service name in your Docker Compose file
+  host: process.env.PG_HOST, // This should match the service name in the Docker Compose file
   dialect: 'postgres',
 });
 
--- dockerfiles/README.md
+++ dockerfiles/README.md
@@ -3,9 +3,3 @@
 2. To connect to mongodb questiondb for question-service, edit the following env variable in the file:
 - `MONGOURL`: mongodb connection string
 3. Run docker-compose up
-
-# setup for cloud postgres user-db:
-1. To connect to elephantsql user database, edit the following env variable in the file:
-- `POSTGRESURL`: postgres connection string
-2. Edit dbConfig file in backend/user-service/src/db so that sequelize uses the connection string.
-3. Run docker-compose up
