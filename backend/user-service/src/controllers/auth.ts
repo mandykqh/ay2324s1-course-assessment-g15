896a05ed378fc1dee74f94ffc9661e7729c9189a
jerrelllzw <lowjerrell47@gmail.com>
2023-10-04 11:42:37 +0800

    chore: changed createSession to return session instead of message

--- backend/user-service/src/controllers/auth.ts
+++ backend/user-service/src/controllers/auth.ts
@@ -3,7 +3,7 @@ import express from 'express';
 export const createSession = async (req: express.Request, res: express.Response) => {
     try {
         req.session.userId = req.body.userId;
-        return res.status(200).json({ message: 'Session created successfully' });
+        return res.status(200).json(req.session);
     } catch (error) {
         console.error('Error creating session', error);
         return res.status(500).send('Internal server error');
