66c2abee0965ccdedba2676f8f2b71f965dd732d
adriel kee <adrielkee@gmail.com>
2023-10-21 11:59:48 +0800

    Fix: dequeueing bug for multi-cat selection

--- backend/matching-service/src/services/rabbitmq-service.ts
+++ backend/matching-service/src/services/rabbitmq-service.ts
@@ -7,11 +7,11 @@ export const rabbitMQSetup = async () => {
   try{
     const connection = await amqp.connect(process.env.RABBITMQ_URL);
     const channel = await connection.createChannel();
-    await channel.assertQueue(`confirmation_Queue`, { durable: false });
+    channel.assertQueue(`confirmation_Queue`, { durable: false });
     for (const category of categoryEnum) {
       for (const complexity of complexityEnum) {
         const queueName = `${category}_${complexity}`;
-        await channel.assertQueue(queueName, { durable: false });
+        channel.assertQueue(queueName, { durable: false });
         console.log(`Channel and Queue created for ${category}_${complexity}`);
       }
     }
