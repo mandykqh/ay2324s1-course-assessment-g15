1fb2e7ef4d566440158fabf7594fed044e94f755
Dylan Sng <dsja612@gmail.com>
2023-10-26 20:34:25 +0800

    chore: setup env variables for frontend for deployment

--- frontend/src/configs.tsx
+++ frontend/src/configs.tsx
@@ -1,6 +1,5 @@
-export const QUESTIONS_SERVICE_URL = 'http://localhost:8080/questions';
-export const USERS_SERVICE_URL = "http://localhost:5000/users";
-export const AUTH_SERVICE_URL = "http://localhost:5000/auth";
-export const MATCHING_SERVICE_URL = "http://localhost:3000/";
-export const COLLABORATION_SERVICE_URL = "http://localhost:4000/";
-export const HISTORY_SERVICE_URL = "http://localhost:7000/";
+export const QUESTIONS_SERVICE_URL = import.meta.env.VITE_QUESTIONS_SERVICE_URL || 'http://localhost:8080/questions';
+export const USERS_SERVICE_URL = import.meta.env.VITE_USERS_SERVICE_URL || "http://localhost:5000/users";
+export const AUTH_SERVICE_URL = import.meta.env.VITE_AUTH_SERVICE_URL || "http://localhost:5000/auth";
+export const MATCHING_SERVICE_URL = import.meta.env.VITE_MATCHING_SERVICE_URL || "http://localhost:3000/";
+export const COLLABORATION_SERVICE_URL = import.meta.env.VITE_COLLABORATION_SERVICE_URL || "http://localhost:4000/";
