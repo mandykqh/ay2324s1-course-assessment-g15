6f5d599933c863a9645a8f4bc4b857f7352a2944
mandykqh <>
2023-11-11 08:59:19 +0800

    style: revamp nav bar

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text, Center, Tab, TabList, TabPanel, TabPanels, Tabs, Spacer, Image, Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { HStack, Divider, Tag, Box, Flex, Text, Center, Tab, TabList, TabPanel, TabPanels, Tabs, Spacer, Image, Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { SECONDARY_COLOR } from "../CommonStyles";
 import { useNavigate } from "react-router-dom";
 import LocalStorageHandler from "../handlers/LocalStorageHandler";
@@ -29,35 +29,40 @@ const NavigationBar: React.FC<Props> = ({ index }) => {
   ]
 
   function signoutHandler() {
-// <<<<<<< HEAD
-//     AuthRequestHandler.signout()
-//       .then(() => {
-//         LocalStorageHandler.clearUserData();
-//         navigate('..');
-//       })
-//       .catch((e) => {
-//         console.log(e);
-//       });
-//   }
-// =======
-		AuthRequestHandler.signout(LocalStorageHandler.getUserData()!.username)
-			.then(() => {
-				LocalStorageHandler.clearUserData();
-				navigate('..');
-			})
-			.catch((e) => {
-				console.log(e);
-			});
-	}
-// >>>>>>> master
+    // <<<<<<< HEAD
+    //     AuthRequestHandler.signout()
+    //       .then(() => {
+    //         LocalStorageHandler.clearUserData();
+    //         navigate('..');
+    //       })
+    //       .catch((e) => {
+    //         console.log(e);
+    //       });
+    //   }
+    // =======
+    AuthRequestHandler.signout(LocalStorageHandler.getUserData()!.username)
+      .then(() => {
+        LocalStorageHandler.clearUserData();
+        navigate('..');
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+  // >>>>>>> master
 
   return (
-    <Box w={'100%'} h={HEIGHT} backgroundColor={SECONDARY_COLOR} position={'absolute'}>
+    <Box w={'100%'} h={HEIGHT} bg='primary.blue1' position={'absolute'}>
       <Flex>
         <Center h={HEIGHT} ml={150}>
-          <Box onClick={() => navigate('../home')} cursor={'pointer'}>
-            <Text as='b' fontSize={25} mr={50}>PeerPrep</Text>
-          </Box>
+          <HStack>
+            <Box onClick={() => navigate('../home')} cursor={'pointer'}>
+              <Image h={30} src='src\assets\logo.svg' />
+            </Box>
+            <Box onClick={() => navigate('../home')} cursor={'pointer'}>
+              <Text as='b' color='primary.green' fontSize={25} mr={50}>PeerPrep</Text>
+            </Box>
+          </HStack>
           <Tabs variant={'line'} index={index}>
             <TabList>
               {tabHeadings.map(h => <Tab key={h.label} onClick={h.onClick}>{h.label}</Tab>)}
@@ -68,6 +73,13 @@ const NavigationBar: React.FC<Props> = ({ index }) => {
         <Menu>
           <MenuButton>
             <Center h={HEIGHT} mr={50} cursor={'pointer'}>
+              {LocalStorageHandler.getUserData()?.role === 'ADMIN' && (
+                <>
+                  <Tag mx='15px' colorScheme="cyan">Administrator</Tag>
+                  <Divider orientation="vertical" />
+                </>
+              )}
+              <Text textStyle='h1' px='15px'>{LocalStorageHandler.getUserData()?.username}</Text>
               <Image h={30} src='src\assets\images\user.png' />
             </Center>
           </MenuButton>
