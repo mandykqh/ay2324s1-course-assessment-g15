c65fa12edb1c743a34516e151a5c5a5d755871af
Wei-Xinn <E0544325@u.nus.edu>
2023-11-12 02:01:14 +0800

    chore: refactor UserAuthenticated page

--- frontend/src/pages/UserAuthentication.tsx
+++ frontend/src/pages/UserAuthentication.tsx
@@ -1,5 +1,5 @@
 import { Box, Flex, Tab, TabList, Tabs, useToast } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import WelcomeLogo from "../components/user/userAuthentication/WelcomeLogo";
 import LoginCard from "../components/user/userAuthentication/LoginCard";
 import SignUpCard from "../components/user/userAuthentication/SignUpCard";
@@ -32,99 +32,112 @@ function LoginPage() {
   const toast = useToast();
   const navigate = useNavigate();
 
-  // If user is authenticated: Saves user data and navigates to home if user is authenticated
-  // Else, display error
-  function loginHandler() {
-    AuthRequestHandler.login(loginUserName, loginPassword)
-      .then((result) => {
-        saveUserData(result);
-        navigate('home');
-      }).catch(() =>
-        showError('Invalid Credentials', toast)
-      );
-  }
-
   function toggleView() {
     setDisplayLoginForm(!displayLoginForm);
     setDisplaySignupForm(!displaySignupForm);
   }
 
-  function signUpHandler() {
-    const requiredFields = [
-      { value: email, message: 'Email cannot be empty' },
-      { value: signUpUsername, message: 'Username cannot be empty' },
-      { value: signUpPassword, message: 'Password cannot be empty' },
-    ];
-
-    const emptyField = requiredFields.find(field => !field.value);
-    if (emptyField) {
-      showError(emptyField.message, toast);
-      return;
+  function renderLoginCard() {
+    const loginHandler = () => {
+      AuthRequestHandler.login(loginUserName, loginPassword)
+        .then((result) => {
+          saveUserData(result);
+          navigate('home');
+        }).catch(() =>
+          showError('Invalid Credentials', toast)
+        );
     }
 
-    if (signUpPassword !== confirmPassword) {
-      showError("Passwords do not match", toast);
-      return;
-    }
-
-    UserRequestHandler.createUser(signUpUsername, email, signUpPassword)
-      .then(() => {
-        showSuccess('You are now registered! proceed to login', toast);
-        toggleView();
-      }).catch((e: any) => showError(e.response.data.message, toast));
+    return (
+      <LoginCard
+        username={loginUserName}
+        usernameSetter={setLoginUsername}
+        password={loginPassword}
+        passwordSetter={setLoginPassword}
+        loginHandler={loginHandler}
+      />
+    );
   }
 
-  const tabs = [
-    { label: 'Login', onClick: () => toggleView() },
-    { label: 'Sign Up', onClick: () => toggleView() },
-  ]
+  function renderSignUpCard() {
+    const signUpHandler = () => {
+      const requiredFields = [
+        { value: email, message: 'Email cannot be empty' },
+        { value: signUpUsername, message: 'Username cannot be empty' },
+        { value: signUpPassword, message: 'Password cannot be empty' },
+      ];
 
-  authChecker(setIsAuthenticated);
-  if (isAuthenticated) {
+      const emptyField = requiredFields.find(field => !field.value);
+      if (emptyField) {
+        showError(emptyField.message, toast);
+        return;
+      }
+
+      if (signUpPassword !== confirmPassword) {
+        showError("Passwords do not match", toast);
+        return;
+      }
+
+      UserRequestHandler.createUser(signUpUsername, email, signUpPassword)
+        .then(() => {
+          showSuccess('You are now registered! proceed to login', toast);
+          toggleView();
+        }).catch((e: any) => showError(e.response.data.message, toast));
+    }
+
+    return (
+      <SignUpCard
+        username={signUpUsername}
+        usernameSetter={setSignUpUsername}
+        password={signUpPassword}
+        passwordSetter={setSignUpPassword}
+        confirmPassword={confirmPassword}
+        confirmPasswordSetter={setConfirmPassword}
+        email={email}
+        emailSetter={setEmail}
+        signUpHandler={signUpHandler}
+      />
+    );
+  }
+
+  function renderTabs() {
+    const tabs = [
+      { label: 'Login', onClick: () => toggleView() },
+      { label: 'Sign Up', onClick: () => toggleView() },
+    ]
+    return (
+      <Tabs w={'200px'} bg={'rgb(45, 55, 72)'} variant={'line'} borderRadius={5}>
+        <TabList>
+          {tabs.map((tab) =>
+            <Tab w={'100px'} onClick={tab.onClick} key={tab.label}>
+              {tab.label}
+            </Tab>
+          )}
+        </TabList>
+      </Tabs>
+    );
+  }
+
+  function redirectAuthenticated() {
     navigate('../home');
     return;
   }
 
-  return (
-    <>
+  function renderLoginPage() {
+    return (
       <Flex justify="center" align="center" minHeight="100vh">
         <WelcomeLogo />
         <Box w='100%'>
-          <Tabs w={'200px'} bg={'rgb(45, 55, 72)'} variant={'line'} borderRadius={5}>
-            <TabList>
-              {tabs.map((tab) =>
-                <Tab w={'100px'} onClick={tab.onClick} key={tab.label}>
-                  {tab.label}
-                </Tab>
-              )}
-            </TabList>
-          </Tabs>
-          {displayLoginForm &&
-            <LoginCard
-              username={loginUserName}
-              usernameSetter={setLoginUsername}
-              password={loginPassword}
-              passwordSetter={setLoginPassword}
-              loginHandler={loginHandler}
-            />
-          }
-          {displaySignupForm &&
-            <SignUpCard
-              username={signUpUsername}
-              usernameSetter={setSignUpUsername}
-              password={signUpPassword}
-              passwordSetter={setSignUpPassword}
-              confirmPassword={confirmPassword}
-              confirmPasswordSetter={setConfirmPassword}
-              email={email}
-              emailSetter={setEmail}
-              signUpHandler={signUpHandler}
-            />
-          }
+          {renderTabs()}
+          {displayLoginForm && renderLoginCard()}
+          {displaySignupForm && renderSignUpCard()}
         </Box>
       </Flex>
-    </>
-  );
+    );
+  }
+
+  authChecker(setIsAuthenticated);
+  return isAuthenticated ? redirectAuthenticated() : renderLoginPage();
 }
 
 export default LoginPage;
