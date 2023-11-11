57d5c00c25960d72b7f46c8815feae582addd953
mandykqh <>
2023-11-11 23:14:14 +0800

    style: navbar padding

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -52,47 +52,46 @@ const NavigationBar: React.FC<Props> = ({ index }) => {
   // >>>>>>> master
 
   return (
-    <Box w={'100%'} h={HEIGHT} bg='primary.blue1' position={'absolute'}>
-      <Flex>
-        <Center h={HEIGHT} ml={150}>
-          <HStack>
-            <Box onClick={() => navigate('../home')} cursor={'pointer'}>
-              <Image h={30} src='src\assets\logo.svg' />
-            </Box>
-            <Box onClick={() => navigate('../home')} cursor={'pointer'}>
-              <Text as='b' color='primary.green' fontSize={25} mr={50}>PeerPrep</Text>
-            </Box>
-          </HStack>
-          <Tabs variant={'line'} index={index}>
-            <TabList>
-              {tabHeadings.map(h =>
-                <Tab color='primary.green'
-                  textStyle='h1'
-                  key={h.label} onClick={h.onClick}>{h.label}
-                </Tab>)}
-            </TabList>
-          </Tabs>
-        </Center>
-        <Spacer />
-        <Menu>
-          <MenuButton>
-            <Center h={HEIGHT} mr={50} cursor={'pointer'}>
-              {LocalStorageHandler.getUserData()?.role === 'ADMIN' && (
-                <>
-                  <Tag mx='15px' colorScheme="cyan">Administrator</Tag>
-                  <Divider orientation="vertical" />
-                </>
-              )}
-              <Text textStyle='h1' px='15px'>{LocalStorageHandler.getUserData()?.username}</Text>
-              <Image h={30} src='src\assets\images\user.png' />
-            </Center>
-          </MenuButton>
-          <MenuList>
-            <MenuItem onClick={() => navigate('../profile')}>Edit profile</MenuItem>
-            <MenuItem onClick={signoutHandler}>Sign out</MenuItem>
-          </MenuList>
-        </Menu>
-      </Flex >
+    <Box w={'100%'} bg='primary.blue1' position={'absolute'} pt={0.6} pb={1.5}>      <Flex>
+      <Center h={HEIGHT} ml={150}>
+        <HStack>
+          <Box onClick={() => navigate('../home')} cursor={'pointer'}>
+            <Image h={30} src='src\assets\logo.svg' />
+          </Box>
+          <Box onClick={() => navigate('../home')} cursor={'pointer'}>
+            <Text as='b' color='primary.green' fontSize={25} mr={50}>PeerPrep</Text>
+          </Box>
+        </HStack>
+        <Tabs variant={'line'} index={index}>
+          <TabList>
+            {tabHeadings.map(h =>
+              <Tab color='primary.green'
+                textStyle='h1'
+                key={h.label} onClick={h.onClick}>{h.label}
+              </Tab>)}
+          </TabList>
+        </Tabs>
+      </Center>
+      <Spacer />
+      <Menu>
+        <MenuButton>
+          <Center h={HEIGHT} mr={50} cursor={'pointer'}>
+            {LocalStorageHandler.getUserData()?.role === 'ADMIN' && (
+              <>
+                <Tag mx='15px' colorScheme="cyan">Administrator</Tag>
+                <Divider orientation="vertical" />
+              </>
+            )}
+            <Text textStyle='h1' px='15px'>{LocalStorageHandler.getUserData()?.username}</Text>
+            <Image h={30} src='src\assets\images\user.png' />
+          </Center>
+        </MenuButton>
+        <MenuList>
+          <MenuItem onClick={() => navigate('../profile')}>Edit profile</MenuItem>
+          <MenuItem onClick={signoutHandler}>Sign out</MenuItem>
+        </MenuList>
+      </Menu>
+    </Flex >
     </Box >
   );
 }
